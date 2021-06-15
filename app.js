4c230e417b36e4f1c16458ff8071dc635ea08b0a
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-06-15 17:45:23 +0530

    nodeapi-integration

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 // app.use(express.json());
 
 const corsOptions = {
-  origin: corsUrl,
+  origin: "http://localhost:3000",
 };
 // app.options("*", cors());
 // app.use(cors(corsOptions));
