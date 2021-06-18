77484e565e87aabdc84bf25c1227def9d50c7592
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-06-18 18:35:30 +0530

    blog-edit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 //  origin: "https://vic.vercel.app",
 // origin:"http://localhost:3000"
 const corsOptions = {
-  origin: "https://vic.vercel.app",
+  origin: "http://localhost:3000",
 };
 app.options("*", cors());
 app.use(cors(corsOptions));
